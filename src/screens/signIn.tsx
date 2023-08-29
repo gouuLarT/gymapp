e3aa9bd6067e8ef18bca743000734a9305804f8c
Davi <davi@MBP-de-Diana.lan>
2023-08-29 11:20:32 -0300

    importing svg logo and heading

--- src/screens/signIn.tsx
+++ src/screens/signIn.tsx
@@ -1,17 +1,33 @@
-import { VStack, Image } from "native-base";
+import { VStack, Image, Text, Center, Heading } from "native-base";
 
 import backgroundImg from "@assets/background.png";
+import LogoSvg from "@assets/logo.svg"
 
-export function SignIn(){
-    return(
+export function SignIn() {
+    return (
         <VStack flex={1} bg="gray.700">
-            <Image 
-            source={backgroundImg}
-            alt="people training"
-            position="absolute"
-            resizeMode="contain"
+            <Image
+                source={backgroundImg}
+                alt="people training"
+                position="absolute"
+                resizeMode="contain"
             />
-       
+
+            <Center my={24}>
+                <LogoSvg />
+
+                <Text color={"gray.100"} fontSize="sm">
+                    Trainee your mind and body
+                </Text>
+            </Center>
+
+            <Center>
+                <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+                    Access your account
+                </Heading>
+            </Center>
+        
+
         </VStack>
     )
 }
