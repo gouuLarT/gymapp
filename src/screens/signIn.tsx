b040234ca8e1f6e3d99321c3f2eeb10e3ffb4252
Davi <davi@MBP-de-Diana.lan>
2023-08-30 14:17:06 -0300

    Applying Authentication Navigation

--- src/screens/signIn.tsx
+++ src/screens/signIn.tsx
@@ -1,15 +1,22 @@
 import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
-
+import { AuthNavigatorRoutesProps } from '@routes/auth.routes'
 import backgroundImg from "@assets/background.png";
 import LogoSvg from "@assets/logo.svg"
 import { Input } from "@components/Input";
 import { Button } from "@components/button";
+import { useNavigation } from "@react-navigation/native";
 
 export function SignIn() {
+
+    const navigation = useNavigation<AuthNavigatorRoutesProps>();
+
+    function handleNewAccount(){
+        navigation.navigate("signUp")
+    }
+
     return (
         <ScrollView contentContainerStyle={{ flexGrow: 1}} showsVerticalScrollIndicator={false}>
         <VStack flex={1} px={10}>
-
             <Image
                 source={backgroundImg}
                 alt="people training"
@@ -56,7 +63,9 @@ export function SignIn() {
 
                 <Button 
                 title="Register" 
-                variant='outline' />
+                variant='outline' 
+                onPress={handleNewAccount}
+                />
 
             </Center>
 
--- src/screens/signUp.tsx
+++ src/screens/signUp.tsx
@@ -4,8 +4,16 @@ import backgroundImg from "@assets/background.png";
 import LogoSvg from "@assets/logo.svg"
 import { Input } from "@components/Input";
 import { Button } from "@components/button";
+import { useNavigation } from "@react-navigation/native"; 
 
 export function SignUp() {
+
+    const navigation = useNavigation();
+
+    function handleGoBack(){
+        navigation.goBack()
+    }
+
     return (
         <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
             <VStack flex={1} px={10}>
@@ -51,6 +59,7 @@ export function SignUp() {
                     title="Back to login"
                     variant='outline'
                     mt={24}
+                    onPress={handleGoBack}
                 />
             </VStack>
         </ScrollView>
