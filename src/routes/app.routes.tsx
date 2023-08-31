f26080b94c563caa0ace01feba1d836acb3fa8d5
Davi <davi@MBP-de-Diana.lan>
2023-08-31 09:13:51 -0300

    Changing Icons of the Bottom Navigator.

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -6,6 +6,12 @@ import { History } from '@screens/history'
 import { Home } from '@screens/home'
 import { Profile } from '@screens/profile'
 
+import { useTheme } from "native-base";
+
+import HomeSvg from '@assets/home.svg'
+import ProfileSvg from '@assets/profile.svg'
+import HistorySvg from '@assets/history.svg'
+
 type AppRoutes = {
     home: undefined
     exercise: undefined
@@ -17,7 +23,13 @@ export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>
 
 const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>()
 
+
 export function AppRoutes(){
+
+    const { sizes } = useTheme()
+    
+    const IconSize = sizes[6]
+    
     return(
         <Navigator screenOptions={{
             headerShown: false,
@@ -26,14 +38,41 @@ export function AppRoutes(){
             <Screen 
             name="home"
             component={Home}
+            options={{
+                tabBarIcon: ({ color }) => (
+                    <HomeSvg 
+                    fill={color}
+                    width={IconSize}
+                    height={IconSize}
+                    />
+                )
+            }}
             />
             <Screen 
             name="history"
             component={History}
+            options={{
+                tabBarIcon: ({ color }) => (
+                    <HistorySvg 
+                    fill={color}
+                    width={IconSize}
+                    height={IconSize}
+                    />
+                )
+            }}
             />
             <Screen 
             name="profile"
             component={Profile}
+            options={{
+                tabBarIcon: ({ color }) => (
+                    <ProfileSvg 
+                    fill={color}
+                    width={IconSize}
+                    height={IconSize}
+                    />
+                )
+            }}
             />
             <Screen 
             name="exercise"
