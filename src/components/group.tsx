02962dc778759a7cf23d2c4e7bce38c0c54e1ddb
Davi <davi@MBP-de-Diana.lan>
2023-08-31 15:28:56 -0300

    Creating the Group component and utilizing the Pressable

--- src/components/group.tsx
+++ src/components/group.tsx
@@ -1,4 +1,4 @@
-import { Text } from "native-base";
+import { Pressable, Text } from "native-base";
 
 type Props = {
     name: string;
@@ -6,6 +6,16 @@ type Props = {
 
 export function Group({name}: Props){
     return(
+        <Pressable 
+        bg="gray.600"
+        mr={3}
+        w={24}
+        h={10}
+        rounded="md"
+        justifyContent="center"
+        alignItems="center"
+        overflow="hidden"
+        >
         <Text 
         color="gray.200"
         textTransform="uppercase"
@@ -14,5 +24,6 @@ export function Group({name}: Props){
         >
             {name}
         </Text>
+        </Pressable>
     );
 }
