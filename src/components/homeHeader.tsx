005da87635616ba1a77cc56a26b3c7c05926962b
Davi <davi@MBP-de-Diana.lan>
2023-08-31 11:11:22 -0300

    Styling the HomeHeader

--- src/components/homeHeader.tsx
+++ src/components/homeHeader.tsx
@@ -2,13 +2,19 @@ import { Heading, VStack, HStack, Text } from "native-base"
 
 export function HomeHeader() {
     return (
-        <HStack>
+        <HStack 
+        bgColor='gray.600'
+        pt={16}
+        alignItems='center'
+        pb={5}
+        px={8}
+        >
         <VStack>
-                <Text color='gray.100'>
+                <Text color='gray.100' fontSize="md">
                     Hello,
                 </Text>
                 
-                <Heading color='gray.100'>
+                <Heading color='gray.100' fontSize="md">
                     Davi Duarte
                 </Heading>
         </VStack>
