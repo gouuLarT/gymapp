33add4c7620724c8f2c586e52bb7781f18b0f8df
Davi <davi@MBP-de-Diana.lan>
2023-09-01 15:31:49 -0300

    Empty List Message

--- src/screens/history.tsx
+++ src/screens/history.tsx
@@ -1,23 +1,22 @@
 import { HistoryCard } from '@components/historyCard'
 import { ScreenHeader } from '@components/screenHeader'
-import { Heading, SectionList, VStack } from 'native-base'
+import { Heading, SectionList, VStack, Text } from 'native-base'
 import { useState } from 'react'
 
 export function History() {
     const [exercises, setExercises] = useState([
         {
-            title: '01.09.2023',
-            data: ["Single arm-row", "Front pull down"]
+            title: '01.09.23',
+            data: ["Front Pull Down", "Single-arm Row" ]
         },
         {
-            title: '05.10.2023',
-            data: ["Front pull down"]
+            title: '05.10.23',
+            data: ["Front Pull Down"]
         }
-    ])
-
+    ]);
 
     return (
-        <VStack>
+        <VStack flex={1}>
             <ScreenHeader title='History' />
 
             <SectionList
@@ -27,13 +26,18 @@ export function History() {
                     <HistoryCard />
                 )}
                 renderSectionHeader={({ section }) => (
-                    <Heading color="gray.200" fontSize="md" mb="3" mt={3}>
+                    <Heading color="gray.200" fontSize="md" mb="3" mt={10}>
                         {section.title}
                     </Heading>
                 )}
                 px={8}
+                contentContainerStyle={exercises.length === 0 && { flex: 1, justifyContent: 'center'}}
+                ListEmptyComponent={() => (
+                    <Text color="gray.100" textAlign="center">
+                        Shall we train today?
+                    </Text>
+                )}
             />
-
         </VStack>
-    )
+    );
 }
--- src/screens/profile.tsx
+++ src/screens/profile.tsx
@@ -51,6 +51,7 @@ export function Profile() {
 
                 <Button
                     title="Update"
+                    
                 />
 
 
