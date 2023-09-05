77c29ecf554739cddef0ba3558908b22bad64ee8
Davi <davi@MBP-de-Diana.lan>
2023-09-04 22:49:34 -0300

    Obtendo a mensagem de erro

--- src/screens/signUp.tsx
+++ src/screens/signUp.tsx
@@ -9,120 +9,122 @@ import { Input } from "@components/Input";
 import { Button } from "@components/button";
 
 type FormDataProps = {
-    name: string,
-    email: string,
-    password: string,
-    password_confirm: string
+  name: string;
+  email: string;
+  password: string;
+  password_confirm: string;
 }
 
 export function SignUp() {
+  
+  const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>();
 
-    const { control, handleSubmit } = useForm<FormDataProps>();
+  const navigation = useNavigation();
 
-    const navigation = useNavigation();
+  function handleGoBack() {
+    navigation.goBack();
+  }
 
-    function handleGoBack() {
-        navigation.goBack();
-    }
+  function handleSignUp({ name, email, password, password_confirm }: FormDataProps) {
+    console.log({ name, email, password, password_confirm })
+  }
 
-    function handleSignUp(data: any) {
-        console.log({ data })
-    }
+  return (
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
+        <VStack flex={1} px={10} pb={16}>
+        <Image 
+          source={BackgroundImg}
+          defaultSource={BackgroundImg}
+          alt="Pessoas treinando"
+          resizeMode="contain"
+          position="absolute"
+        />
 
-    return (
-        <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
-            <VStack flex={1} px={10} pb={16}>
-                <Image
-                    source={BackgroundImg}
-                    defaultSource={BackgroundImg}
-                    alt="Pessoas treinando"
-                    resizeMode="contain"
-                    position="absolute"
-                />
+        <Center my={24}>
+          <LogoSvg />
 
-                <Center my={24}>
-                    <LogoSvg />
+          <Text color="gray.100" fontSize="sm">
+            Treine sua mente e o seu corpo.
+          </Text>
+        </Center>
 
-                    <Text color="gray.100" fontSize="sm">
-                        Treine sua mente e o seu corpo.
-                    </Text>
-                </Center>
+        <Center>
+          <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+            Crie sua conta
+          </Heading>
 
-                <Center>
-                    <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
-                        Crie sua conta
-                    </Heading>
+          <Controller 
+            control={control}
+            name="name"
+            rules={{
+              required: 'Informe o nome.'
+            }}
+            render={({ field: { onChange, value } }) => (
+              <Input 
+                placeholder="Nome"
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+          />
 
-                    <Controller
-                        control={control}
-                        name="name"
-                        rules={{
-                            required:'Informe o nome'
-                        }}
-                        render={({ field: { onChange, value } }) => (
-                            <Input
-                                placeholder="Nome"
-                                onChangeText={onChange}
-                                value={value}
-                            />
-                        )}
-                    />
+          <Text color="white">{errors.name?.message}</Text>
 
-                    <Controller
-                        control={control}
-                        name="email"
-                        render={({ field: { onChange, value } }) => (
-                            <Input
-                                placeholder="E-mail"
-                                keyboardType="email-address"
-                                autoCapitalize="none"
-                                onChangeText={onChange}
-                                value={value}
-                            />
-                        )}
-                    />
+          <Controller 
+            control={control}
+            name="email"
+            render={({ field: { onChange, value } }) => (
+              <Input 
+                placeholder="E-mail" 
+                keyboardType="email-address"
+                autoCapitalize="none"
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+          />
 
-                    <Controller
-                        control={control}
-                        name="password"
-                        render={({ field: { onChange, value } }) => (
-                            <Input
-                                placeholder="Senha"
-                                secureTextEntry
-                                onChangeText={onChange}
-                                value={value}
-                            />
-                        )}
-                    />
+          <Controller 
+            control={control}
+            name="password"
+            render={({ field: { onChange, value } }) => (
+              <Input 
+                placeholder="Senha" 
+                secureTextEntry
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+          />
 
-                    <Controller
-                        control={control}
-                        name="password_confirm"
-                        render={({ field: { onChange, value } }) => (
-                            <Input
-                                placeholder="Confirmar a Senha"
-                                secureTextEntry
-                                onChangeText={onChange}
-                                value={value}
-                                onSubmitEditing={handleSubmit(handleSignUp)}
-                                returnKeyType="send"
-                            />
-                        )}
-                    />
+          <Controller 
+            control={control}
+            name="password_confirm"
+            render={({ field: { onChange, value } }) => (
+              <Input 
+                placeholder="Confirmar a Senha" 
+                secureTextEntry
+                onChangeText={onChange}
+                value={value}
+                onSubmitEditing={handleSubmit(handleSignUp)}
+                returnKeyType="send"
+              />
+            )}
+          />
 
-                    <Button
-                        title="Criar e acessar"
-                        onPress={handleSubmit(handleSignUp)}
-                    />
-                </Center>
-
-                <Button
-                    title="Voltar para o login"
-                    variant="outline"
-                    mt={24}
-                    onPress={handleGoBack}
-                />
-            </VStack>
-        </ScrollView>
-    );
+          <Button 
+            title="Criar e acessar" 
+            onPress={handleSubmit(handleSignUp)}
+          />
+        </Center>
+        
+        <Button 
+          title="Voltar para o login" 
+          variant="outline" 
+          mt={24}
+          onPress={handleGoBack}
+        />
+      </VStack>
+    </ScrollView>
+  );
 }
